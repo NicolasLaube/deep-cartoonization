1028add61c1e420817c19917fd897b009933009e
AntoineSTU <antoine.stutz@student-cs.fr>
2022-01-12 21:08:03 +0100

    Added valid to train test split

--- src/dataset/dataset_pictures.py
+++ src/dataset/dataset_pictures.py
@@ -13,10 +13,13 @@ class PicturesDataset(ImageLoader):
         transform: callable,
         nb_images: int = -1,
         train: bool = True,
+        test: bool = False,
     ) -> None:
         self.train = train
         if train:
             csv_path = config.PICTURES_TRAIN_CSV
         else:
+            csv_path = config.PICTURES_VALIDATION_CSV
+        if test:
             csv_path = config.PICTURES_TEST_CSV
         ImageLoader.__init__(self, csv_path, filter_data, transform, nb_images)
