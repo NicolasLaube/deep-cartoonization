7de85888e63183ecddc507c727f6590d1c6c0783
AntoineSTU <antoine.stutz@student-cs.fr>
2022-03-23 01:55:46 +0100

    Updated loss function

--- src/models/trainer_fixed_gan.py
+++ src/models/trainer_fixed_gan.py
@@ -212,7 +212,7 @@ class FixedCartoonGANTrainer(Trainer):
                 # with torch.autocast(self.device):
                 disc_fake = self.discriminator(gen_cartoons)
 
-                gen_bce_loss = bce_loss(disc_fake, fake)
+                gen_bce_loss = bce_loss(disc_fake, real)
                 gen_content_loss = content_loss(gen_cartoons, pictures)
                 gen_loss = gen_bce_loss + gen_content_loss
 
