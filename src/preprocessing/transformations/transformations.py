7eb2330ff0c1b2dbadc6be399baa6a68818634f9
AntoineSTU <antoine.stutz@student-cs.fr>
2022-04-09 23:06:04 +0200

    Fixed sth

--- src/preprocessing/transformations/transformations.py
+++ src/preprocessing/transformations/transformations.py
@@ -75,7 +75,7 @@ class Transform:
     ) -> NDArray[(Any, Any), np.int32]:
         """Transform images (functions that are common to both frame and picture preprocessing)"""
         image = resize.resize(image, self.new_size, self.crop_mode)
-        if self.smoothing_kernel_size > 0:
+        if self.smoothing_kernel_size is not None and self.smoothing_kernel_size > 0:
             image = smooth.edge_promoting(image, kernel_size=self.smoothing_kernel_size)
 
         return self.normalizer.normalize(image)
